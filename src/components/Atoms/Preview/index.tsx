ee9dae51bc0f5aac49df65344ebe2d1cc177218e
xiaoxuan <xiaoxuanpam0117@gmail.com>
2023-05-09 23:40:36 +0800

    fix: correct format

--- src/components/Atoms/Preview/index.tsx
+++ src/components/Atoms/Preview/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import classnames from "classnames";
 
-import {buildBoard} from "../../../model/board";
+import { buildBoard } from "../../../model/board";
 
 import BoardCell from "../BoardCell";
 
@@ -20,7 +20,14 @@ export default function Preview(props: IPreviewProps) {
 
   return (
     <div className={styles.preview} style={boardStyles}>
-      {buildBoard({ row: 4, column: 4, position: {y: 0, x: 0}, shape: props.shape, className: props.className }).map((row, index) =>
+      {buildBoard({
+        row: 4,
+        column: 4,
+        collided: false,
+        position: { y: 0, x: 0 },
+        shape: props.shape,
+        className: props.className,
+      }).map((row, index) =>
         row.map((cell, index) => (
           <BoardCell
             key={index}
