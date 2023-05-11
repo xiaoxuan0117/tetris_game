16c0b5f03e39eeed565084cce08019bbf585bd06
xiaoxuan <xiaoxuanpam0117@gmail.com>
2023-05-11 19:54:14 +0800

    fix: correct game height

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -12,6 +12,19 @@ export default function Game() {
   const isGameOver = useAppSelector(
     (state: RootState) => state.board.isGameOver
   );
+
+  React.useEffect(() => {
+    // ref: https://ithelp.ithome.com.tw/articles/10249090
+    let windowsVH = window.innerHeight / 100;
+    document
+      .getElementById("game")
+      ?.style.setProperty("--vh", windowsVH + "px");
+    window.addEventListener("resize", function () {
+      document
+        .getElementById("game")
+        ?.style.setProperty("--vh", windowsVH + "px");
+    });
+  });
   return (
     <div id="game" className={styles.game}>
       {isGameOver ? <Landing /> : <Tetris />}
